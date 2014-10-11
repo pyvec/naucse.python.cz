750b7271a4b701971d7050b0ea21ab7ae1ef17d7
Petr Viktorin <encukou@gmail.com>
2014-10-11 12:07:55 +0200

    Add second session, without index

---
+++ js/solutions.js
@@ -0,0 +1,34 @@
+(function () {
+    var details = document.getElementsByTagName('details');
+    function get_show_func(detail) {
+        return function () {
+            console.log('data-innerhtml');
+            detail.innerHTML = detail.getAttribute('data-innerhtml');
+            detail.setAttribute('style', '');
+        }
+    }
+    function show_all(detail) {
+        for(i=0; i < details.length; i++) {
+            get_show_func(details[i])();
+        }
+        window.location.hash = '#showall';
+    }
+    for(i=0; i < details.length; i++) {
+        details[i].setAttribute('data-innerhtml', details[i].innerHTML);
+        details[i].setAttribute('style', 'margin-bottom:100em');
+        var btn = document.createElement('input');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('class', 'btn');
+        btn.setAttribute('value', 'Řešení »');
+        btn.addEventListener('click', get_show_func(details[i]));
+        console.log(get_show_func(details[i]));
+        details[i].innerHTML = '';
+        details[i].appendChild(btn);
+    }
+    document.getElementById('show-all').addEventListener('click', show_all);
+    if(window.location.hash.contains('showall')) {
+        show_all();
+    }
+    document.body.addEventListener('copy', function () {return false;});
+    document.body.addEventListener('cut', function () {return false;});
+})()
