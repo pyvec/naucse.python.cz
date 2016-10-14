8d187b8b746167813eb8433a947f7a88da5b1e16
Petr Viktorin <encukou@gmail.com>
2016-10-14 23:09:04 +0200

    Remove the "v1" directory
    These files are all still on pyladies.cz; don't need them here

--- v1/s005-modules/piskvorky1d/piskvorky.py
+++
@@ -1,4 +0,0 @@
-def tah(pole, index, symbol):
-    if pole[index] != '-':
-        raise ValueError('Pole {} je obsazeno symbolem {}'.format(index, pole[index]))
-    return pole[:index] + symbol + pole[index + 1:]
--- v1/s005-modules/piskvorky1d/strategie/nahoda.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def tah_pocitace(pole, symbol):
-    if '-' not in pole:
-        raise ValueError('Plné pole')
-    while True:
-        cislo = random.randrange(len(pole))
-        if pole[cislo] == '-':
-            return pole[:cislo] + symbol + pole[cislo+1:]
