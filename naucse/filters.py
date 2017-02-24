af5e405a1b59b00fdb7a245f26e7f1d7fb57d949
Petr Viktorin <encukou@gmail.com>
2017-02-24 16:59:04 +0100

    Remove workaround for Markdown entities

--- naucse/filters.py
+++ naucse/filters.py
@@ -10,9 +10,6 @@ def convert_markdown(text, inline=False):
     text = dedent(text)
     result = Markup(markdown(text))
 
-    # Markdown code blocks are translated literally, this solves problem with entities.
-    result = result.replace('&amp;', '&').replace('&gt;', '>').replace('&#39;', "'").replace('&#34;', '"').replace('&lt;', '<')
-
     if inline and result.startswith('<p>') and result.endswith('</p>'):
         result = result[len('<p>'):-len('</p>')]
 
