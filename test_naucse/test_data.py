0160b44f0a4e0aa9ac5b6d75ab3d126d942a059d
Miro Hrončok <miro@hroncok.cz>
2017-09-25 14:02:25 +0200

    Add PyLadies Praha Autumn (CZ.NIC + NTK)
    Merges https://github.com/pyvec/naucse.python.cz/pull/206

--- test_naucse/test_data.py
+++ test_naucse/test_data.py
@@ -2,8 +2,21 @@ import calendar
 
 from naucse import models
 
+import pytest
 
-def test_2017_mipyt_zima_is_only_on_wednesdays():
-    run = models.Course('.', 'runs/2017/mipyt-zima')
+
+root = models.Root('.')
+
+
+@pytest.mark.parametrize(('slug', 'weekday'), [
+                             ('runs/2017/mipyt-zima',
+                              calendar.WEDNESDAY),
+                             ('runs/2017/pyladies-praha-podzim-cznic',
+                              calendar.TUESDAY),
+                             ('runs/2017/pyladies-praha-podzim-ntk',
+                              calendar.MONDAY),
+                         ])
+def test_run_is_only_on_given_weekday(slug, weekday):
+    run = models.Course(root, slug)
     for session in run.sessions.values():
-        assert session.date.weekday() == calendar.WEDNESDAY
+        assert session.date.weekday() == weekday
