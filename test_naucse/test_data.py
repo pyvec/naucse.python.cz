3a068f3b2ffea78a6a1cc261e2db76107bc5bb54
Miro Hrončok <miro@hroncok.cz>
2017-09-25 13:13:20 +0200

    Add PyLadies Praha Autumn CZ.NIC

--- test_naucse/test_data.py
+++ test_naucse/test_data.py
@@ -3,7 +3,16 @@ import calendar
 from naucse import models
 
 
+root = models.Root('.')
+
+
 def test_2017_mipyt_zima_is_only_on_wednesdays():
-    run = models.Course('.', 'runs/2017/mipyt-zima')
+    run = models.Course(root, 'runs/2017/mipyt-zima')
     for session in run.sessions.values():
         assert session.date.weekday() == calendar.WEDNESDAY
+
+
+def test_2017_pyladies_praha_podzim_cznic_is_only_on_tuesdays():
+    run = models.Course(root, 'runs/2017/pyladies-praha-podzim-cznic')
+    for session in run.sessions.values():
+        assert session.date.weekday() == calendar.TUESDAY
