f0c15fe0a0eb1f1fab3e16d6aa976054ad94a1b0
Petr Viktorin <encukou@gmail.com>
2018-10-31 16:50:45 +0100

    naucse_render: Render lessons and pages as relative URLs

--- naucse_render/page.py
+++ naucse_render/page.py
@@ -6,6 +6,16 @@ from .load import read_yaml
 from .templates import environment, vars_functions
 from .markdown import convert_markdown
 
+def lesson_url(lesson_name, *, page='index'):
+    if page == 'index':
+        return f'../../../{lesson_name}/'
+    else:
+        return f'../../../{lesson_name}/{page}/'
+
+
+def subpage_url(subpage_name):
+    return f'./{subpage_name}/'
+
 
 def render_page(lesson_slug, page):
     lesson_directory = Path('lessons', lesson_slug)
@@ -20,8 +30,8 @@ def render_page(lesson_slug, page):
     text = path.read_text(encoding='utf-8')
 
     text = env.get_template(page_name).render(
-        lesson_url=lambda a, **kw: '',  # XXX
-        subpage_url=lambda a: '',  # XXX
+        lesson_url=lesson_url,
+        subpage_url=subpage_url,
         **{'$solutions': [], **vars_functions(vars)},
     )
     if info['style'] == 'md':
