6bc9cc72f74fca57cae503867da1b0de84ed66e9
Petr Viktorin <encukou@gmail.com>
2018-12-31 16:27:39 +0100

    Test dumping an empty model

--- test_naucse/test_model.py
+++ test_naucse/test_model.py
@@ -1,11 +1,27 @@
 from pathlib import Path
 
 import pytest
+import yaml
 
 from naucse import models
 
 
 fixture_path = Path(__file__).parent / 'fixtures'
+
+
+def dummy_schema_url_factory(cls, **kwargs):
+    return f'http://dummy.test/schema/{cls.__name__}'
+
+
+def assert_matches_expected_dump(data, filename):
+    yaml_path = fixture_path / 'expected-dumps' / filename
+    expected_yaml = yaml_path.read_text()
+    expected = yaml.safe_load(expected_yaml)
+    if data != expected:
+        # Comparing structures dumped to YAML
+        data_dump = yaml.safe_dump(data, default_flow_style=False, indent=4)
+        assert data_dump == expected_yaml
+        assert data == expected
 
 
 def test_empty_model():
@@ -31,3 +47,8 @@ def test_licenses():
     assert model.licenses['cc-by-sa-40'].slug == 'cc-by-sa-40'
     assert model.licenses['cc-by-sa-40'].url.endswith('/licenses/by-sa/4.0/')
     assert model.licenses['cc-by-sa-40'].title.endswith('4.0 International')
+
+
+def test_dump_empty_model():
+    model = models.Root(schema_url_factory=dummy_schema_url_factory)
+    assert_matches_expected_dump(models.dump(model), 'empty-root.yml')
