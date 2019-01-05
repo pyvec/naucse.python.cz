76851346bb30513d5131ca731a61dca10da9bc2d
Petr Viktorin <encukou@gmail.com>
2019-01-05 22:02:45 +0100

    Adjust test_session_times to the new models

--- test_naucse/test_session_times.py
+++ test_naucse/test_session_times.py
@@ -1,51 +1,49 @@
 import datetime
-from pathlib import Path
 
 import pytest
 import dateutil.tz
 
 from naucse import models
+from test_naucse.conftest import fixture_path
 
 
 TZINFO = dateutil.tz.gettz('Europe/Prague')
 
 
-@pytest.fixture
-def model():
-    path = Path(__file__).parent / 'fixtures/test_content'
-    return models.Root(path)
-
-
 def test_run_with_times(model):
-    run = model.runs[2000, 'run-with-times']
-    assert run.default_start_time == datetime.time(19, 00, tzinfo=TZINFO)
-    assert run.default_end_time == datetime.time(21, 00, tzinfo=TZINFO)
+    model.load_local_courses(fixture_path / 'test_content')
+
+    run = model.courses['2000/run-with-times']
+    assert run.default_time == {
+        'start': datetime.time(19, 00, tzinfo=TZINFO),
+        'end': datetime.time(21, 00, tzinfo=TZINFO),
+    }
 
     lesson = run.sessions['normal-lesson']
     assert lesson.date == datetime.date(2000, 1, 1)
-    assert lesson.start_time == datetime.datetime(2000, 1, 1, 19, 00,
-                                                  tzinfo=TZINFO)
-    assert lesson.end_time == datetime.datetime(2000, 1, 1, 21, 00,
-                                                tzinfo=TZINFO)
+    assert lesson.time == {
+        'start': datetime.datetime(2000, 1, 1, 19, tzinfo=TZINFO),
+        'end': datetime.datetime(2000, 1, 1, 21, tzinfo=TZINFO),
+    }
 
 
 def test_run_without_times(model):
-    run = model.runs[2000, 'run-without-times']
-    assert run.default_start_time is None
-    assert run.default_end_time is None
+    model.load_local_courses(fixture_path / 'test_content')
+
+    run = model.courses['2000/run-without-times']
+    assert run.default_time is None
 
     lesson = run.sessions['normal-lesson']
     assert lesson.date == datetime.date(2000, 1, 1)
-    assert lesson.start_time is None
-    assert lesson.end_time is None
+    assert lesson.time is None
 
 
 def test_course(model):
-    course = model.courses['normal-course']
-    assert course.default_start_time is None
-    assert course.default_end_time is None
+    model.load_local_courses(fixture_path / 'test_content')
+
+    course = model.courses['courses/normal-course']
+    assert course.default_time is None
 
     lesson = course.sessions['normal-lesson']
     assert lesson.date is None
-    assert lesson.start_time is None
-    assert lesson.end_time is None
+    assert lesson.time is None
