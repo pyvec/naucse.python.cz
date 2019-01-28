29ab96459a10c15873c16458776401de2933414f
Petr Viktorin <encukou@gmail.com>
2019-01-28 15:41:13 +0100

    Refactoring and API (#505)
    Add JSON API with two flavors: input (what we get from "forks") and output
      (available at at `/v0/naucse.json`)
    * The API is described and validated using jsonschema.
    * Uses HTML for content, never Markdown.
    * Relative URLs use a custom `naucse:` schema for the API. This gets converted
      in the HTML "sanitization" step.
    * The API is v0; consumers are expected to coordinate with us until it is
      finalized.
    
    
    The part that should be in "forks" is moved to the `naucse_render` module,
    which became its own repo & PyPI package.
    
    
    When one course's build fails, everything fails. No silent "an error occured"
    pages.
    * Only fully applies in "freeze mode" (with `NAUCSE_FREEZE=1`, or Flask
      in non-debug mode).
    * In debug mode, lessons are rendered on demand instead of at startup.
    
    
    Doesn't do fancy guessing of repo URL&branch for edit info.
    Use `NAUCSE_MAIN_REPO_URL` and `NAUCSE_MAIN_REPO_BRANCH` if needed
    (e.g. on Travis).
    
    
    The model is better defined. Notably:
    * A *course* has a list of *sessions*
    * A session has a list of *materials*
    * A material may be for a *lesson* (or an external link)
    * A course (!) has *lessons* (which are referenced by materials).
      (All lessons are in a course; there's a special hidden `lessons` course
      containing the "local"/"canonical" lessons.)
    
    
    The distinction between forks and local content is disappearing (but still
    present).
    
    
    The distinction between "courses" and "runs" in the model is disappearing:
    mostly they just have different slugs (e.g. `courses/mi-pyt` vs.
    `2018/pyladies-brno-jaro`) and are in different collections for lookup.
    
    
    Instead of `FORKS_ENABLED=true`, there's `NAUCSE_TRUSTED_REPOS='*'`.
    You can filter allowed repos a bit (docs TODO).
    
    
    The model is different (`converters.py` is utilities for stuff you can
    load/dump from/to a versioned JSON API).
    
    
    Pages request LaTeX using "`modules: {'katex': '0.7.1'}`.
    This should become a general mechanism to request some extra functionality
    (like JS/CSS).
    
    
    Model objects have info about their URLs.
    
    ---
    
    PR & review: https://github.com/pyvec/naucse.python.cz/pull/505
    Helps resolve: https://github.com/pyvec/naucse.python.cz/issues/494
    
    Thanks to Miro Hrnočok, Petr Messner and Mikuláš Poul for review and minor fixes.

--- naucse/urlconverters.py
+++ naucse/urlconverters.py
@@ -1,15 +1,6 @@
-from functools import partial
+import re
 
-from flask import abort
 from werkzeug.routing import BaseConverter
-
-from naucse.models import Lesson
-
-
-class ModelConverter(BaseConverter):
-    def __init__(self, model, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.model = model
 
 
 _converters = {}
@@ -22,67 +13,49 @@ def _converter(name):
     return decorator
 
 
+def register_url_converters(app):
+    for name, cls in _converters.items():
+        app.url_map.converters[name] = cls
+
 @_converter('course')
-class CourseConverter(ModelConverter):
-    regex = r'([0-9]{4}|course)/[^/]+'
+class CourseConverter(BaseConverter):
+    regex = r'(([0-9]{4}|course)/[^/]+)|lessons'
+
+    # XXX: The URLs should really be "courses/<...>",
+    # but we don't have good redirects yet,, so leave them at
+    # "course/<...>"
 
     def to_python(self, value):
-        year, slug = value.split('/')
-        if year == "course":
-            return self.model.courses[slug]
-        else:
-            try:
-                return self.model.runs[int(year), slug]
-            except KeyError:
-                abort(404)
+        if value.startswith('course/'):
+            value = value.replace('course/', 'courses/')
+        return value
 
     def to_url(self, value):
-        if isinstance(value, str):
-            if "/" not in value:  # XXX
-                value = "course/"+value
-            value = self.to_python(value)
-
-        # the converter can be called with a dict mimicking a course
-        if isinstance(value, dict):
-            return value["slug"]
-
-        return value.slug
-
-
-@_converter('lesson_slug')
-class LessonSlugConverter(ModelConverter):
-    regex = r'[^/]+/[^/]+'
-
-    def to_url(self, value):
-        if isinstance(value, Lesson):
-            return value.slug
-
-        # the converter can be called with a dict mimicking a lesson
-        elif isinstance(value, dict):
-            return value["slug"]
-
+        if value.startswith('courses/'):
+            return value.replace('courses/', 'course/', 1)
         return value
 
 
 @_converter('lesson')
-class LessonConverter(LessonSlugConverter):
+class LessonConverter(BaseConverter):
+    regex = r'[^/]+/[^/]+'
 
     def to_python(self, value):
-        try:
-            return self.model.get_lesson(value)
-        except LookupError:
-            abort(404)
+        return value
 
     def to_url(self, value):
-        # the converter can be called with a dict mimicking a lesson
-        if isinstance(value, dict):
-            return value["slug"]
-
-        if isinstance(value, str):
-            value = self.to_python(value)
-        return value.slug
+        return value
 
 
-def register_url_converters(app, model):
-    for name, cls in _converters.items():
-        app.url_map.converters[name] = partial(cls, model)
+@_converter('is_input')
+class IsInputConverter(BaseConverter):
+    regex = r'in|out'
+
+    def to_python(self, value):
+        return (value == 'in')
+
+    def to_url(self, is_input):
+        if is_input:
+            return 'in'
+        else:
+            return 'out'
---
+++ test_naucse/__init__.py
@@ -0,0 +1,6 @@
+# This is a package.
+
+# For a long time, pytest discouraged test packages, but no longer;  see:
+# https://docs.pytest.org/en/latest/goodpractices.html#tests-outside-application-code
+# This makes it easy for test_naucse.conftest to also include helpers that
+# aren't fixtures.
